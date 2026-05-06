1cdd47e6b7ec44b8511bd2a98437edfcf97dd745
predasoli.SVG <predasolienzo@gmail.com>
2026-05-06 11:25:04 -0300

    meu primeiro programa phyton

---
+++ 1.1-numeros-em-phyton.py
@@ -0,0 +1,37 @@
+# AULA COMPLETA: NUMERO EM PHYTON
+"""
+VAMOS APREDER:
+1-TIPOS NUMÉRICOS
+2-CONVERSÕES DE TIPOS
+3-HIERARQUIA NUMÉRICA
+4-OPERAÇÕES MATMÁTICAS
+4-COERÇÃO DE TIPOS
+5-VERIFICÇÃO DE TIPOS
+6-ENTRADA DE DADOS
+"""
+
+# PASSO 1 - TIPOS NUMÉRICOS #
+# INT -> INTEIROS #
+# FLOAT -> NÚMERICOS COM CASAS DECIMAIS #
+# COMPLEX -> NÚMEROS COMPLEXOS (usado em matematica/engenharia) #
+
+print(" --TIPOS NÚMERICOS-- ")
+vendas = 1000
+
+custo = 300
+lucro = vendas - custo
+print ("o nosso lucro foi de:", lucro, "e a nossas vendas foi de:", vendas)
+numeros_interio =10 
+#mostrando o valor!!
+print("valor:", numeros_interio)
+
+#TYPE () mostra qual é o tipo da variavel
+print("tipo:", type (numeros_interio))
+
+
+
+       
+
+
+
+
