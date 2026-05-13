02a0a86cd406f547e1ca9beb0413cf0b0f76d320
predasoli-SVG <predasolienzo@gmail.com>
2026-05-13 20:31:29 -0300

    Update 1.1-numeros-em-phyton.py

--- 1.1-numeros-em-phyton.py
+++ 1.1-numeros-em-phyton.py
@@ -46,3 +46,39 @@ print("tipo:", type(numero_decimal))
 numero_complexo = 2+3j
 print("valor:", numero_complexo)
 print("tipo:", type(numero_complexo))
+
+#git clone e copiar o link
+
+#exemplo 03 - acessando cada parte do número 
+
+# .real retorna a parte real 
+print("parte real:", numero_complexo.real)
+#.img retorna a parte imaginária 
+print ("parte imaginária:", numero_complexo.imag)
+
+# apenas para separar visualmente a saída no terminal
+print("\n\n")
+  
+  #ddaos vindos do usuario são textos (string), muitas vezes é nescessario converter eles
+
+print("=== conversão ====")
+
+# float -> int
+valor =int (3.9)
+print("int(3.9):", valor)
+print("tipo:", type(valor))
+ 
+ #string -> int
+valor1 = "10"
+print(type, (valor1))
+
+valor2 =int ("10")
+print('int ("10"):', valor2)
+print("tipo:", type(valor2))
+
+#int --> float
+valor3 = float(10)
+print("valor(10):", valor3)
+print("tipo:", type(valor3))
+
+
